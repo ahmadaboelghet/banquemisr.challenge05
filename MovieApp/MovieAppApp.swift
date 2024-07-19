b52270bf99a9504ce0b51f912a612479ebec4193
Ahmad Aboelghet <agmadaboelghet@gmail.com>
2024-07-19 19:05:45 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ahmad Aboelghet on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
